3b7dc834694c5fc8dd65857c5e29b274fe5a1a45
gonAccount <35288746+gonAccount@users.noreply.github.com>
2022-05-31 11:58:23 +0200

    Add vector entry locating methods
    Add methods for finding entries and obtaining entries at given positions

--- Vector.java
+++ Vector.java
@@ -1,48 +1,69 @@
 package MyMathLib;
 
 import java.util.Arrays;
-import java.util.function.Predicate;
+import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
 public class Vector {
-    private final int[] values;
+    static private final String ERROR_UNEQUAL_SIZE = "Unequally sized vectors.";
+
+    public final int[] values;
+    public int length;
 
     public Vector(int... values) {
         this.values = values;
+        this.length = values.length;
     }
 
-    public Vector plus(Vector another) {
+    public Vector plus(Vector another) throws Exception {
+        if(this.length != another.length) {
+            throw new Exception(ERROR_UNEQUAL_SIZE);
+        }
         int[] added = IntStream.range(0, this.values.length).map(i -> this.values[i] + another.values[i]).toArray();
         return new Vector(added);
     }
 
-    public Vector minus(Vector another) {
+    public Vector minus(Vector another) throws Exception {
+        if(this.length != another.length) {
+            throw new Exception(ERROR_UNEQUAL_SIZE);
+        }
         int[] added = IntStream.range(0, this.values.length).map(i -> this.values[i] - another.values[i]).toArray();
         return new Vector(added);
     }
 
-    public Vector times(Vector another) {
+    public Vector times(Vector another) throws Exception {
+        if(this.length != another.length) {
+            throw new Exception(ERROR_UNEQUAL_SIZE);
+        }
         int[] added = IntStream.range(0, this.values.length).map(i -> this.values[i] * another.values[i]).toArray();
         return new Vector(added);
     }
 
-    public static int dot(Vector x1, Vector x2) {
-        return Arrays.stream(x1.times(x2).values).sum();
+    public Vector which(IntPredicate predicate) {
+        IntStream range = IntStream.range(0, this.values.length);
+        int [] valueArray = range.filter(i -> predicate.test(this.values[i])).toArray();
+        return new Vector(valueArray);
     }
 
-    public int[] where(Predicate predicate) {
-        // TODO Return indexes where a statement holds.
-        return null;
+    public Vector at(int... positions) {
+        // TODO Handle negative values for the entries of "positions". Either allow x.at(-1) to represent last element or not.
+        int [] valueArray = Arrays.stream(positions).map(i -> this.values[i]).toArray();
+        return new Vector(valueArray);
     }
 
-    public static int[] at(Vector mathVector) {
-        // TODO Return indexes where a statement holds.
-        return null;
+    public Vector at(Vector vector) {
+        return at(vector.values);
     }
 
     public void print() {
         Arrays.stream(values).forEach(x -> System.out.print(x + ", "));
-        System.out.println();
+    }
+
+    public static int dot(Vector x1, Vector x2) throws Exception {
+        if(x1.length != x2.length) {
+            throw new Exception(ERROR_UNEQUAL_SIZE);
+        }
+        return Arrays.stream(x1.times(x2).values).sum();
     }
 
     @Override
--- VectorTest.java
+++ VectorTest.java
@@ -8,23 +8,38 @@ public class VectorTest {
     Vector x2 = new Vector(4, 0, 6);
 
     @Test
-    public void testAddition() {
+    public void testAddition() throws Exception {
         Assert.assertEquals(x1.plus(x2), new Vector(4, 1, 6));
     }
 
     @Test
-    public void testSubtraction() {
+    public void testAddition2() throws Exception {
+        Assert.assertEquals(x1.plus(x2), new Vector(4, 1, 6));
+    }
+
+    @Test
+    public void testSubtraction() throws Exception {
         Assert.assertEquals(x1.minus(x2), new Vector(-4, 1, -6));
     }
 
     @Test
-    public void testMultiplication() {
+    public void testMultiplication() throws Exception {
         Assert.assertEquals(x1.times(x2), new Vector(0, 0, 0));
     }
 
     @Test
-    public void testDotProduct() {
+    public void testDotProduct() throws Exception {
         Assert.assertEquals(Vector.dot(x1, x2), 0);
+    }
+
+    @Test
+    public void testWhich() {
+        Assert.assertEquals(x1.which(x -> x == 0), new Vector(0, 2));
+    }
+
+    @Test
+    public void testAt() {
+        Assert.assertEquals(x1.at(0, 2), new Vector(0, 0));
     }
 
 }
