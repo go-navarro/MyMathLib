eab4c4f303205ad15f48eca8154cafc1242897bc
gonAccount <35288746+gonAccount@users.noreply.github.com>
2022-06-01 11:38:40 +0200

    Add method to Vector
    Add an instance method which applies a function to a Vector's elements

--- Vector.java
+++ Vector.java
@@ -1,69 +1,83 @@
 package MyMathLib;
 
 import java.util.Arrays;
-import java.util.function.IntPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Vector {
     static private final String ERROR_UNEQUAL_SIZE = "Unequally sized vectors.";
 
-    public final int[] values;
+    public final double[] values;
     public int length;
 
-    public Vector(int... values) {
+    public Vector(double... values) {
         this.values = values;
         this.length = values.length;
     }
 
     public Vector plus(Vector another) throws Exception {
-        if(this.length != another.length) {
+        if (this.length != another.length) {
             throw new Exception(ERROR_UNEQUAL_SIZE);
         }
-        int[] added = IntStream.range(0, this.values.length).map(i -> this.values[i] + another.values[i]).toArray();
+        double[] added = IntStream.range(0, this.values.length).mapToDouble(i -> this.values[i] + another.values[i]).toArray();
         return new Vector(added);
     }
 
     public Vector minus(Vector another) throws Exception {
-        if(this.length != another.length) {
+        if (this.length != another.length) {
             throw new Exception(ERROR_UNEQUAL_SIZE);
         }
-        int[] added = IntStream.range(0, this.values.length).map(i -> this.values[i] - another.values[i]).toArray();
+        double[] added = IntStream.range(0, this.values.length).mapToDouble(i -> this.values[i] - another.values[i]).toArray();
+        return new Vector(added);
+    }
+
+    public Vector div(Vector another) throws Exception {
+        if (this.length != another.length) {
+            throw new Exception(ERROR_UNEQUAL_SIZE);
+        }
+        double[] added = IntStream.range(0, this.values.length).mapToDouble(i -> this.values[i] / another.values[i]).toArray();
         return new Vector(added);
     }
 
     public Vector times(Vector another) throws Exception {
-        if(this.length != another.length) {
+        if (this.length != another.length) {
             throw new Exception(ERROR_UNEQUAL_SIZE);
         }
-        int[] added = IntStream.range(0, this.values.length).map(i -> this.values[i] * another.values[i]).toArray();
+        double[] added = IntStream.range(0, this.values.length).mapToDouble(i -> this.values[i] * another.values[i]).toArray();
         return new Vector(added);
     }
 
-    public Vector which(IntPredicate predicate) {
+    public Vector which(Predicate<Double> predicate) {
         IntStream range = IntStream.range(0, this.values.length);
-        int [] valueArray = range.filter(i -> predicate.test(this.values[i])).toArray();
+        double[] valueArray = range.filter(i -> predicate.test(this.values[i])).mapToDouble(x -> x).toArray();
         return new Vector(valueArray);
     }
 
     public Vector at(int... positions) {
-        // TODO Handle negative values for the entries of "positions". Either allow x.at(-1) to represent last element or not.
-        int [] valueArray = Arrays.stream(positions).map(i -> this.values[i]).toArray();
+        double[] valueArray = Arrays.stream(positions).mapToDouble(i -> this.values[i]).toArray();
         return new Vector(valueArray);
     }
 
     public Vector at(Vector vector) {
-        return at(vector.values);
+        int[] positions = Arrays.stream(vector.values).mapToInt(x -> (int) x).toArray();
+        return at(positions);
     }
 
     public void print() {
         Arrays.stream(values).forEach(x -> System.out.print(x + ", "));
     }
 
-    public static int dot(Vector x1, Vector x2) throws Exception {
-        if(x1.length != x2.length) {
+    public static double dot(Vector x1, Vector x2) throws Exception {
+        if (x1.length != x2.length) {
             throw new Exception(ERROR_UNEQUAL_SIZE);
         }
         return Arrays.stream(x1.times(x2).values).sum();
+    }
+
+    public Vector applyFunction(Function<Double, Double> fun) {
+        double[] doubles = Arrays.stream(this.values).map(fun::apply).toArray();
+        return new Vector(doubles);
     }
 
     @Override
--- VectorTest.java
+++ VectorTest.java
@@ -4,42 +4,46 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class VectorTest {
+    final static double ERROR_TOLERANCE = 5E-10;
+
     Vector x1 = new Vector(0, 1, 0);
     Vector x2 = new Vector(4, 0, 6);
+    Vector x3 = new Vector(10, 100, 1000, 1000000000);
 
     @Test
     public void testAddition() throws Exception {
-        Assert.assertEquals(x1.plus(x2), new Vector(4, 1, 6));
-    }
-
-    @Test
-    public void testAddition2() throws Exception {
-        Assert.assertEquals(x1.plus(x2), new Vector(4, 1, 6));
+        Assert.assertEquals(new Vector(4, 1, 6), x1.plus(x2));
     }
 
     @Test
     public void testSubtraction() throws Exception {
-        Assert.assertEquals(x1.minus(x2), new Vector(-4, 1, -6));
+        Assert.assertEquals(new Vector(-4, 1, -6), x1.minus(x2));
     }
 
     @Test
     public void testMultiplication() throws Exception {
-        Assert.assertEquals(x1.times(x2), new Vector(0, 0, 0));
+        Assert.assertEquals(new Vector(0, 0, 0), x1.times(x2));
     }
 
     @Test
     public void testDotProduct() throws Exception {
-        Assert.assertEquals(Vector.dot(x1, x2), 0);
+        Assert.assertEquals(0.00, Vector.dot(x1, x2), ERROR_TOLERANCE);
     }
 
     @Test
     public void testWhich() {
-        Assert.assertEquals(x1.which(x -> x == 0), new Vector(0, 2));
+        Assert.assertEquals(new Vector(0, 2), x1.which(x -> x == 0));
     }
 
     @Test
     public void testAt() {
-        Assert.assertEquals(x1.at(0, 2), new Vector(0, 0));
+        Assert.assertEquals(new Vector(0, 0), x1.at(0, 2));
+    }
+
+    @Test
+    public void testApplyFunction() {
+        Vector y3 = x3.applyFunction(x -> Math.log10(x));
+        Assert.assertEquals(new Vector(1.00, 2.00, 3.00, 9.00), y3);
     }
 
 }
